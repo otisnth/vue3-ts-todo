58b66c20e9fa17fa4b79afa7fe8ce423b1bcd7fd
otisnth <otisnth@gmail.com>
2025-02-05 11:27:30 +0300

    feat: добавил кнопки на форму добавления

--- src/features/AddNote/ui/AddNoteForm.vue
+++ src/features/AddNote/ui/AddNoteForm.vue
@@ -1,10 +1,22 @@
 <template>
     <div class="add-note-form">
         <NoteForm />
+
+        <div class="add-note-form__button-row">
+            <Button class="add-note-form__button" :outlined="true">
+                <img class="add-note-form__button-icon" src="/cancel.svg" alt="Cancel icon" />
+                <span class="add-note-form__button-text">Отмена</span>
+            </Button>
+            <Button class="add-note-form__button">
+                <img class="add-note-form__button-icon" src="/edit.svg" alt="List icon" />
+                <span class="add-note-form__button-text">Добавить</span>
+            </Button>
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
+import Button from "@/shared/ui/Button.vue";
 import NoteForm from "@/entities/Note/ui/NoteForm.vue";
 </script>
 
@@ -13,5 +25,11 @@ import NoteForm from "@/entities/Note/ui/NoteForm.vue";
     display: flex;
     flex-direction: column;
     align-items: center;
+    gap: 40px;
+}
+
+.add-note-form__button-row {
+    display: flex;
+    gap: 20px;
 }
 </style>
---
+++ src/shared/ui/Button.vue
@@ -0,0 +1,33 @@
+<template>
+    <button :class="['button', { button_outlined: outlined }]">
+        <slot></slot>
+    </button>
+</template>
+
+<script setup lang="ts">
+defineProps({
+    outlined: {
+        type: Boolean,
+        default: false,
+    },
+});
+</script>
+
+<style>
+.button {
+    display: flex;
+    gap: 10px;
+    align-items: center;
+    padding: 12px 24px;
+    background-color: #e0621b;
+    border-radius: 8px;
+    color: #d6d6d6;
+    font-size: 16px;
+    border: 1px solid #e0621b;
+}
+
+.button_outlined {
+    background-color: transparent;
+    color: #e0621b;
+}
+</style>
